982443b34cd350f567735c3460129def2f91c1b7
aabccd021 <aabccd021@gmail.com>
2025-08-26 10:48:18 +0700

    Refactor test logic using _.filter and add specific error handling

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,4 @@
 import isEven from "is-even";
 import _ from "lodash";
 
-// Using _.map to create a new array by applying a function to each element
-const squaredNumbers = _.map([1, 2, 3], (num) => num * num);
-
-// Using _.filter to create a new array containing only the elements that satisfy a condition
-const evenNumbers = _.filter([1, 2, 3, 4, 5], (num) => isEven(num));
-
-console.log(`Squared Numbers: [${squaredNumbers}], Even Numbers: [${evenNumbers}]`);
+if (_.filter([1, 2, 3], isEven).at(0) !== 2) throw new Error();
