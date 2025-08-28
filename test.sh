81bfc7841767f628f1e0c5563c9796c2be1fae32
aabccd021 <aabccd021@gmail.com>
2025-08-29 06:11:59 +0700

    ``` refactor: Remove unnecessary files and configurations ```

---
+++ test.sh
@@ -0,0 +1,26 @@
+set -eu
+
+index_js="$(git rev-parse --show-toplevel)/index.js"
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf $tmpdir' EXIT
+cd "$tmpdir" || exit 1
+
+nix run nixpkgs#bun install is-even@1.0.0
+nix run nixpkgs#bun install lodash@github:lodash/lodash#8a26eb4
+nix run nixpkgs#bun install @types/bun@1.2.21
+
+nix run nixpkgs#bun run "$index_js" > ./node_modules.nix
+rm -rf ./node_modules
+cp -Lr "$(nix-build --arg pkgs "import <nixpkgs> {}" ./node_modules.nix --no-out-link)" ./node_modules
+chmod -R a+w ./node_modules
+
+{
+  echo "import isEven from 'is-even';"
+  echo "import _ from 'lodash';"
+  echo "import { expect } from 'bun:test';"
+  echo "expect(_.filter([1, 2, 3], isEven).at(0)).toEqual(2);"
+} > ./test.ts
+
+nix run nixpkgs#bun ./test.ts
+nix run nixpkgs#typescript -- --noEmit ./test.ts
--- test.ts
+++
@@ -1,4 +0,0 @@
-import isEven from "is-even";
-import _ from "lodash";
-
-if (_.filter([1, 2, 3], isEven).at(0) !== 2) throw new Error();
