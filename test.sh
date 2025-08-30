6a9662a863909aaf3dd672dec3ea7d365cba394f
aabccd021 <aabccd021@gmail.com>
2025-08-30 12:27:49 +0700

    Fix spacing issues in test script with conventional message

--- test.sh
+++ test.sh
@@ -8,17 +8,17 @@ cd "$tmpdir" || exit 1
 
 nix run nixpkgs#bun install is-even@1.0.0 lodash@github:lodash/lodash#8a26eb4 @types/bun@1.2.21
 
-nix run nixpkgs#bun run "$index_js" > ./node_modules.nix
+nix run nixpkgs#bun run "$index_js" >./node_modules.nix
 rm -rf ./node_modules
 cp -Lr "$(nix-build --no-out-link ./node_modules.nix)" ./node_modules
 chmod -R u+rwX ./node_modules
 
 {
-  echo "import isEven from 'is-even';"
-  echo "import _ from 'lodash';"
-  echo "import { expect } from 'bun:test';"
-  echo "expect(_.filter([1, 2, 3], isEven).at(0)).toEqual(2);"
-} > ./test.ts
+    echo "import isEven from 'is-even';"
+    echo "import _ from 'lodash';"
+    echo "import { expect } from 'bun:test';"
+    echo "expect(_.filter([1, 2, 3], isEven).at(0)).toEqual(2);"
+} >./test.ts
 
 nix run nixpkgs#bun ./test.ts
 nix run nixpkgs#typescript -- --noEmit ./test.ts
