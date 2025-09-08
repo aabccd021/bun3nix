a6155f6d0ea347e3e5e906f654517c934d7e84fe
aabccd021 <aabccd021@gmail.com>
2025-09-08 12:44:03 +0700

    Refactor packageTextPromise function and add args parsing configuration.

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 
 import * as path from "node:path";
 import * as util from "node:util";
-import { $ } from "bun";
+import * as fs from "node:fs/promises";
+import * as os from "node:os";
+import * as child_process from "node:child_process";
 
-async function packageTextPromise([name, value]) {
+async function packageTextPromise(pkg, cwd) {
+  const [name, value] = pkg;
   const [val0, _val1, _val2, val3] = value;
 
   // git dependencies
   if (val3 === undefined) {
     const url = new URL(val0.replace(`${name}@`, "").replace("github:", "https://github.com/"));
-    const bunTagFile = Bun.file(`node_modules/${name}/.bun-tag`);
+    const bunTagFile = Bun.file(`${cwd}/node_modules/${name}/.bun-tag`);
     const bunTag = await bunTagFile.text();
     try {
       await bunTagFile.delete();
-      const hash = await $`nix-hash --base64 --type sha512 --sri node_modules/${name}`.text();
+      const hash = child_process.execSync(`nix-hash --base64 --type sha512 --sri ${cwd}/node_modules/${name}`, { encoding: "utf-8" });
       return [
         `"${name}" = pkgs.fetchgit {`,
         `  url = "${url.origin}${url.pathname}";`,
@@ -39,20 +42,38 @@ async function packageTextPromise([name, value]) {
   ];
 }
 
-const { values: args } = util.parseArgs({
+const arg = util.parseArgs({
   args: process.argv.slice(2),
   options: {
     postinstall: {
       type: "boolean",
+      default: false,
     },
+    package: {
+      type: "string",
+      multiple: true,
+      default: [],
+    }
   },
   strict: true,
 });
 
-const bunLockJsonc = await Bun.file("bun.lock").text();
+if (arg.values.postinstall === false && arg.values.package.length === 0) {
+  process.exit(1);
+}
+
+let cwd = process.cwd();
+if (arg.values.package.length > 0) {
+  cwd = await fs.mkdtemp(path.join(os.tmpdir(), "bun-nix-"));
+  child_process.execSync(`bun add ${arg.values.package.join(" ")}`, { cwd });
+}
+
+const bunLockJsonc = await Bun.file(`${cwd}/bun.lock`).text();
 const bunLockJson = bunLockJsonc.replace(/,(\s*[}\]])/g, "$1");
 const bunLock = JSON.parse(bunLockJson);
-const packageTextPromises = Object.entries(bunLock.packages).map(packageTextPromise);
+const packageTextPromises = Object
+  .entries(bunLock.packages)
+  .map((pkg) => packageTextPromise(pkg, cwd));
 const packageTexts = await Promise.all(packageTextPromises);
 
 const packageText = packageTexts
--- test.sh
+++ test.sh
@@ -1,15 +1,52 @@
+#!/usr/bin/env bash
 set -eu
 
 index_js="$(git rev-parse --show-toplevel)/index.js"
 
+if ! command -v bun >/dev/null 2>&1; then
+    PATH="$(nix build nixpkgs#bun --no-link --print-out-paths 2>/dev/null)/bin:$PATH"
+    export PATH
+fi
+
 (
+    echo "# --postinstall"
+
     tmpdir=$(mktemp -d)
     trap 'rm -rf $tmpdir' EXIT
     cd "$tmpdir" || exit 1
 
-    nix run nixpkgs#bun install is-even@1.0.0 lodash@github:lodash/lodash#8a26eb4 @types/bun@1.2.21
+    bun install is-even@1.0.0 lodash@github:lodash/lodash#8a26eb4 @types/bun@1.2.21
 
-    nix run nixpkgs#bun run "$index_js" >./node_modules.nix
+    "$index_js" --postinstall >./node_modules.nix
+    rm -rf ./node_modules
+    cp -Lr "$(nix-build --no-out-link ./node_modules.nix)" ./node_modules
+    chmod -R u+rwX ./node_modules
+
+    {
+        echo "import isEven from 'is-even';"
+        echo "import _ from 'lodash';"
+        echo "import { expect } from 'bun:test';"
+        echo "expect(_.filter([1, 2, 3], isEven).at(0)).toEqual(2);"
+    } >./test.ts
+
+    nix run nixpkgs#bun ./test.ts
+    nix run nixpkgs#typescript -- --noEmit ./test.ts
+)
+
+(
+    echo "# --package"
+
+    tmpdir=$(mktemp -d)
+    trap 'rm -rf $tmpdir' EXIT
+    cd "$tmpdir" || exit 1
+
+    # --package lodash@github:lodash/lodash#8a26eb4 \
+    "$index_js" \
+        --package "github:lodash/lodash#8a26eb4" \
+        --package "@types/bun@1.2.21" \
+        --package "is-even@1.0.0" \
+        >./node_modules.nix
+    # cat ./node_modules.nix
     rm -rf ./node_modules
     cp -Lr "$(nix-build --no-out-link ./node_modules.nix)" ./node_modules
     chmod -R u+rwX ./node_modules
