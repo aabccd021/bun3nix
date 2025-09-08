51cb197863960d72ab955052ed8b77bc40f1d224
aabccd021 <aabccd021@gmail.com>
2025-09-08 16:40:26 +0700

    refactor: Update packageTextPromise to destructure parameters and use modulePath instead of name.

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ import * as fs from "node:fs/promises";
 import * as os from "node:os";
 import * as child_process from "node:child_process";
 
-async function packageTextPromise(pkg, cwd) {
-  const [name, value] = pkg;
-  const [val0, _val1, _val2, val3] = value;
+async function packageTextPromise({ cwd, pkg, name, baseName, modulePath }) {
+  const [val0, _val1, _val2, val3] = pkg;
 
   // git dependencies
   if (val3 === undefined) {
     const url = new URL(val0.replace(`${name}@`, "").replace("github:", "https://github.com/"));
-    const bunTagFile = Bun.file(`${cwd}/node_modules/${name}/.bun-tag`);
+    const bunTagFile = Bun.file(`${cwd}/node_modules/${modulePath}/.bun-tag`);
     const bunTag = await bunTagFile.text();
     try {
       await bunTagFile.delete();
-      const hash = child_process.execSync(`nix-hash --base64 --type sha512 --sri ${cwd}/node_modules/${name}`, { encoding: "utf-8" });
+      const hash = child_process.execSync(`nix-hash --base64 --type sha512 --sri ${cwd}/node_modules/${modulePath}`, { encoding: "utf-8" });
       return [
-        `"${name}" = pkgs.fetchgit {`,
+        `"${modulePath}" = pkgs.fetchgit {`,
         `  url = "${url.origin}${url.pathname}";`,
         `  rev = "${url.hash.substring(1)}";`,
         `  hash = "${hash.trim()}";`,
@@ -33,13 +32,40 @@ async function packageTextPromise(pkg, cwd) {
   // npm dependencies
   const tarballName = path.basename(val0).replaceAll("@", "-");
   return [
-    `"${name}" = extractTarball (`,
+    `"${modulePath}" = extractTarball (`,
     `  pkgs.fetchurl {`,
-    `    url = "https://registry.npmjs.org/${name}/-/${tarballName}.tgz";`,
+    `    url = "https://registry.npmjs.org/${baseName}/-/${tarballName}.tgz";`,
     `    hash = "${val3}";`,
     `  }`,
     `);`,
   ];
+}
+
+function serialize(cwd, obj) {
+  let result;
+  const dependencyEntries = Object.entries(obj)
+    .map(([name, pkg]) => {
+      const parentName = Object.keys(obj)
+        .filter((n) => name.startsWith(`${n}/`) && n !== name)
+        .sort((a, b) => b.length - a.length)
+        .at(0);
+      const baseName = parentName !== undefined ? name.substring(parentName.length + 1) : name;
+      return [name, { parentName, baseName, pkg }]
+    })
+
+  const dependencyMap = Object.fromEntries(dependencyEntries)
+
+  return Object.entries(dependencyMap)
+    .map(([name, { baseName, pkg }]) => {
+      const modulePaths = []
+      let current = dependencyMap[name];
+      while (current !== undefined) {
+        modulePaths.push(current.baseName);
+        current = dependencyMap[current.parentName];
+      }
+      const modulePath = modulePaths.reverse().join("/node_modules/");
+      return packageTextPromise({ cwd, pkg, name, baseName, modulePath });
+    })
 }
 
 const arg = util.parseArgs({
@@ -73,11 +99,8 @@ if (arg.positionals.length > 0) {
 const bunLockJsonc = await Bun.file(`${cwd}/bun.lock`).text();
 const bunLockJson = bunLockJsonc.replace(/,(\s*[}\]])/g, "$1");
 const bunLock = JSON.parse(bunLockJson);
-const packageTextPromises = Object
-  .entries(bunLock.packages)
-  .map((pkg) => packageTextPromise(pkg, cwd));
+const packageTextPromises = serialize(cwd, bunLock.packages);
 const packageTexts = await Promise.all(packageTextPromises);
-
 const packageText = packageTexts
   .flat()
   .filter((line) => line !== undefined)
@@ -102,6 +125,7 @@ lib.pipe packages [
     name: package: ''
       mkdir -p "$out/\${name}"
       cp -Lr \${package}/* "$out/\${name}"
+      chmod -R u+w "$out/\${name}"
     ''
   ))
   (lib.concatStringsSep "\\n")
