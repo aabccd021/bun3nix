760bd5b41a503c9b09ed6dbc01a25e81fba382d3
aabccd021 <aabccd021@gmail.com>
2025-09-09 07:59:59 +0700

    Update npm package dependencies and format file with nixfmt.

--- test.sh
+++ test.sh
@@ -28,9 +28,9 @@ setup_test() {
 
     bun install \
         is-even@1.0.0 \
-        lodash@github:lodash/lodash#8a26eb4 \
         @types/bun@1.2.21 \
-        nanoid@https://github.com/ai/nanoid
+        github:lodash/lodash#8a26eb4 \
+        https://github.com/ai/nanoid
     bun3nix postinstall >./npm_deps.nix
 
     rm -rf ./node_modules
@@ -38,10 +38,43 @@ setup_test() {
     chmod -R u+rwX ./node_modules
 
     echo '
+        import { strictEqual } from "node:assert";
         import isEven from "is-even";
         import _ from "lodash";
-        import { strictEqual } from "node:assert";
         import { nanoid } from "nanoid";
+        strictEqual(_.filter([1, 2, 3], isEven).at(0), 2);
+        strictEqual(nanoid().length, 21);
+    ' >./test.ts
+    # use nodejs instead of bun because bun sometimes automatically download dependencies
+    # even without package.json, bun.lock, or node_modules
+    nix run nixpkgs#nodejs ./test.ts
+    nix run nixpkgs#typescript -- --noEmit ./test.ts
+
+    cp ./npm_deps.nix ./npm_deps_formatted.nix
+    nix run nixpkgs#nixfmt -- --strict ./npm_deps_formatted.nix
+    diff -u --color=always ./npm_deps.nix ./npm_deps_formatted.nix
+)
+
+(
+    echo "# postinstall: success with non-default package names"
+    setup_test
+
+    bun install \
+        is-even@1.0.0 \
+        @types/bun@1.2.21 \
+        lorem@github:lodash/lodash#8a26eb4 \
+        ipsum@https://github.com/ai/nanoid
+    bun3nix postinstall >./npm_deps.nix
+
+    rm -rf ./node_modules
+    cp -Lr "$(nix-build --no-out-link ./npm_deps.nix)/lib/node_modules" ./node_modules
+    chmod -R u+rwX ./node_modules
+
+    echo '
+        import { strictEqual } from "node:assert";
+        import isEven from "is-even";
+        import _ from "lorem";
+        import { nanoid } from "ipsum";
         strictEqual(_.filter([1, 2, 3], isEven).at(0), 2);
         strictEqual(nanoid().length, 21);
     ' >./test.ts
@@ -61,9 +94,9 @@ setup_test() {
 
     bun3nix install \
         is-even@1.0.0 \
-        lodash@github:lodash/lodash#8a26eb4 \
         @types/bun@1.2.21 \
-        nanoid@https://github.com/ai/nanoid \
+        github:lodash/lodash#8a26eb4 \
+        https://github.com/ai/nanoid \
         >./npm_deps.nix
     for path in ./node_modules ./package.json ./bun.lock ./bun.lockb; do
         if [ -e "$path" ]; then
@@ -75,10 +108,45 @@ setup_test() {
     chmod -R u+rwX ./node_modules
 
     echo '
+        import { strictEqual } from "node:assert";
         import isEven from "is-even";
         import _ from "lodash";
-        import { strictEqual } from "node:assert";
         import { nanoid } from "nanoid";
+        strictEqual(_.filter([1, 2, 3], isEven).at(0), 2);
+        strictEqual(nanoid().length, 21);
+    ' >./test.ts
+    nix run nixpkgs#nodejs ./test.ts
+    nix run nixpkgs#typescript -- --noEmit ./test.ts
+
+    cp ./npm_deps.nix ./npm_deps_formatted.nix
+    nix run nixpkgs#nixfmt -- --strict ./npm_deps_formatted
+    diff -u --color=always ./npm_deps.nix ./npm_deps_formatted.nix
+)
+
+(
+    echo "# install: success with non-default package names"
+    setup_test
+
+    bun3nix install \
+        is-even@1.0.0 \
+        @types/bun@1.2.21 \
+        lorem@github:lodash/lodash#8a26eb4 \
+        ipsum@https://github.com/ai/nanoid \
+        >./npm_deps.nix
+    for path in ./node_modules ./package.json ./bun.lock ./bun.lockb; do
+        if [ -e "$path" ]; then
+            echo "$path should not exist"
+            exit 1
+        fi
+    done
+    cp -Lr "$(nix-build --no-out-link ./npm_deps.nix)/lib/node_modules" ./node_modules
+    chmod -R u+rwX ./node_modules
+
+    echo '
+        import { strictEqual } from "node:assert";
+        import isEven from "is-even";
+        import _ from "lorem";
+        import { nanoid } from "ipsum";
         strictEqual(_.filter([1, 2, 3], isEven).at(0), 2);
         strictEqual(nanoid().length, 21);
     ' >./test.ts
