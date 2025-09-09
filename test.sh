7e450cf63aed593c40624bc7b757603a682d1160
aabccd021 <aabccd021@gmail.com>
2025-09-09 08:04:15 +0700

    Refactor: Update bun3nix path and use 'bun' command instead of 'bun3nix'.

--- test.sh
+++ test.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-tmpdir=$(mktemp -d)
-
-cp "$(git rev-parse --show-toplevel)/index.js" "$tmpdir/bun3nix"
-PATH="$tmpdir:$PATH"
+PATH="$(nix build nixpkgs#bun --no-link --print-out-paths)/bin:$PATH"
 export PATH
-
-if ! command -v bun >/dev/null 2>&1; then
-    bun_pkg=$(nix build nixpkgs#bun --no-link --print-out-paths)
-    PATH="$bun_pkg/bin:$PATH"
-    export PATH
-fi
+bun3nix="$(git rev-parse --show-toplevel)/index.js"
 
 setup_test() {
     tmpdir=$(mktemp -d)
@@ -28,7 +20,7 @@ setup_test() {
         @types/bun@1.2.21 \
         github:lodash/lodash#8a26eb4 \
         https://github.com/ai/nanoid
-    bun3nix postinstall >./npm_deps.nix
+    bun "$bun3nix" postinstall >./npm_deps.nix
 
     rm -rf ./node_modules
     cp -Lr "$(nix-build --no-out-link ./npm_deps.nix)/lib/node_modules" ./node_modules
@@ -61,7 +53,7 @@ setup_test() {
         @types/bun@1.2.21 \
         lorem@github:lodash/lodash#8a26eb4 \
         ipsum@https://github.com/ai/nanoid
-    bun3nix postinstall >./npm_deps.nix
+    bun "$bun3nix" postinstall >./npm_deps.nix
 
     rm -rf ./node_modules
     cp -Lr "$(nix-build --no-out-link ./npm_deps.nix)/lib/node_modules" ./node_modules
@@ -89,7 +81,7 @@ setup_test() {
     echo "# install: success"
     setup_test
 
-    bun3nix install \
+    bun "$bun3nix" install \
         is-even@1.0.0 \
         @types/bun@1.2.21 \
         github:lodash/lodash#8a26eb4 \
@@ -124,7 +116,7 @@ setup_test() {
     echo "# install: success with non-default package names"
     setup_test
 
-    bun3nix install \
+    bun "$bun3nix" install \
         is-even@1.0.0 \
         @types/bun@1.2.21 \
         lorem@github:lodash/lodash#8a26eb4 \
@@ -159,7 +151,7 @@ setup_test() {
     echo "# install: tailwindcss with plugins"
     setup_test
 
-    bun3nix install \
+    bun "$bun3nix" install \
         @iconify/json@2.2.359 \
         @iconify/tailwind4@1.0.6 \
         daisyui@5.0.46 \
@@ -191,7 +183,7 @@ setup_test() {
     echo "# install: tailwindcss with plugins (separate cli)"
     setup_test
 
-    bun3nix install \
+    bun "$bun3nix" install \
         @iconify/json@2.2.359 \
         @iconify/tailwind4@1.0.6 \
         daisyui@5.0.46 \
@@ -199,7 +191,7 @@ setup_test() {
         >./plugin_deps.nix
     plugin_deps=$(nix-build --no-out-link ./plugin_deps.nix)
 
-    bun3nix install @tailwindcss/cli@4.1.11 >./cli_deps.nix
+    bun "$bun3nix" install @tailwindcss/cli@4.1.11 >./cli_deps.nix
     cli_deps=$(nix-build --no-out-link ./cli_deps.nix)
 
     echo '@import "tailwindcss"; @plugin "@iconify/tailwind4"; @plugin "daisyui";' >./style.css
